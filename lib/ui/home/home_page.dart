35db73358999e22599502600ff6a0b154bf01d28
Kacper Slusarczyk <kacper12345678909@gmail.com>
2020-01-18 00:30:10 +0100

    Added new files to new app

---
+++ lib/ui/home/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Text("Hello"),
+          Text("World")
+        ],
+      )
+    );
+  }
+}
